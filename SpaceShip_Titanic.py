5d601ad7a7eda0dc927232fa85e6f2bafb9e9d94
Gabriel Cintra <63664532+cs-Gabriel@users.noreply.github.com>
2025-02-20 15:33:55 -0300

    Analise Exploratória
    Aplicando melhor as bibliotecas de visualização de dados para ampliar a analise exploratória.

--- SpaceShip_Titanic.py
+++ SpaceShip_Titanic.py
@@ -1,11 +1,17 @@
-
+# A biblioteca pantas é importante para a amanipulação de DataFrames
 import pandas as pd
 
+# A principal Biblioteca para utilizarmos o metodo do Random Forest
 from sklearn.ensemble import RandomForestClassifier
 
+# Importante para facilitar a categorização de variaveis
 from sklearn.preprocessing import KBinsDiscretizer
 
+# Biblioteca essencial para a vizualização de dados
 import matplotlib.pyplot as plt
+
+# Biblioteca essencial para a vizualização de dados
+import seaborn as sns
 
 #%%
 #Usando a função read_csv para ler os aquivos de teste e de treinamento
@@ -51,7 +57,6 @@ test.fillna({
     'VRDeck': 0
 }, inplace=True)
 
-
 #%% Categorizando a idade na base de treino
 
 kbin_idade = KBinsDiscretizer(n_bins= 5, encode= 'ordinal', strategy= 'quantile')
@@ -62,8 +67,54 @@ train.loc[:, 'cat_idade'] = kbin_idade.fit_transform(train[['Age']])
 train['CryoSleep'] = train['CryoSleep'].astype(int)
 train['VIP'] = train['VIP'].astype(int)
 
-#excluindo colunas que não ajudam
-#train.drop(columns = ['VRDeck', 'Spa', 'ShoppingMall', 'FoodCourt', 'RoomService'], inplace= True)
+#%% Aprofundando a analize exploratoria
+
+#Gerando um CroosTab para buscar ligação entre as variaveis e a resposta (Targuet)
+
+pd.crosstab(train['CryoSleep'], train['Transported']).plot(kind= 'bar',
+                                                           figsize=(10,5),
+                                                           color= ['salmon', 'lightblue'])
+plt.title('Frequencia de Transpotados em Sono induzido')
+plt.xlabel('0 = Não estava em sono induzido, 1 = Estava em sono induzido')
+plt.ylabel('Amostragem')
+
+# Asim é possivel notar que existe uma relação grande entre estar induzio e ter sido transpotado, evidenciando essa variavel como uma importante para nosso modelo
+
+#%% Vamos fazer o mesmo grafico mas para outra variavel
+
+pd.crosstab(train['VIP'], train['Transported']).plot(kind= 'bar',
+                                                           figsize=(10,5),
+                                                           color= ['salmon', 'lightblue'])
+plt.title('Frequencia de Transpotados VIP')
+plt.xlabel('0 = Não VIP, 1 = VIP')
+plt.ylabel('Amostragem')
+
+# Como a quantidade de VIP é muito baixa, não é possivel afirmar uma relação dessa variavel com nossa Targuet
+
+#%% Vamos fazer o mesmo grafico mas para outra variavel
+
+pd.crosstab(train['cat_idade'], train['Transported']).plot(kind= 'bar',
+                                                           figsize=(10,5),
+                                                           color= ['salmon', 'lightblue'])
+plt.title('Frequencia de Transpotados VIP')
+plt.xlabel('Faixa de idade')
+plt.ylabel('Amostragem')
+
+'''
+É possivel ver que na primerira divizão tem mais transportados e na ultima divizão o numero de não tranpostado supera o de transportado,
+mostrando uma tendencia, assim essa variavel tem relaçao com a Targuet'''
+
+#%% Separando as variaveis para fazer um mapa de calor buscando ligação entre elas
+variaves = train[['Age','CryoSleep','VIP','RoomService','FoodCourt','ShoppingMall','Spa', 'VRDeck']]
+
+#Criando o mapa de calor, não foi possivel encontra nenhuma relação muito relevante.
+corr_matrix = variaves.corr()
+plt.figure(figsize=(15, 10))
+sns.heatmap(corr_matrix,
+            annot=True,
+            linewidths= 0.5,
+            fmt='.2f',
+            cmap='YlGnBu')
 
 #%% Definino as variaveis para treinar a arvore
 X_train = train[['cat_idade', 'CryoSleep', 'VIP']]
